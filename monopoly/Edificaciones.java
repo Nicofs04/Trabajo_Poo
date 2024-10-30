187ef8290bc29a2242c5e25f1b2d121a1b6d67d9
Iagoformoso <iagoforblan@hotmail.com>
2024-10-30 12:26:36 +0100

    edificaciones

---
+++ monopoly/Edificaciones.java
@@ -0,0 +1,59 @@
+package monopoly;
+
+public class Edificaciones {
+
+    //Atributos
+    
+    private int casas;
+    private int hoteles;
+    private int tenis;
+    private int piscina;
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
