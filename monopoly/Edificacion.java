65924cb3acd0a68aa040a6bff3074619242cef24
Iagoformoso <iagoforblan@hotmail.com>
2024-10-30 12:55:56 +0100

    edificacion

---
+++ monopoly/Edificacion.java
@@ -0,0 +1,77 @@
+package monopoly;
+
+public class Edificacion {
+
+    //Atributos
+
+    private String tipo;
+    private Casilla casilla;
+
+    public Edificacion(Casilla casilla,String tipo){
+
+        this.casilla = casilla;
+        this.tipo = tipo;
+        
+    }
+
+    public Casilla getCasilla(){
+        return this.casilla;
+    }
+
+    public void setCasilla(Casilla casilla){
+        this.casilla = casilla;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo=tipo;
+    }
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
